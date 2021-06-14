07098d7d4e3127248ca41c1cdcdc33049c05f57c
dreamnoob <vitaliklio1100@gmail.com>
2021-06-14 14:46:43 +0300

    First commit

---
+++ src/assets/js/app.js
@@ -0,0 +1,43 @@
+//= ../../../node_modules/slick-carousel/slick/slick.js
+
+"use strict";
+
+document.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.tabs__item');
+    const tabsContent = document.querySelectorAll('.tabscontent__item');
+    const tabParent = document.querySelector('.tabs__list');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.remove('show', 'fade');
+            item.classList.add('hide');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('active');
+    }
+    
+    hideTabContent();
+    showTabContent();
+
+    tabParent.addEventListener('click', (e) => {
+        const target = e.target;
+
+        if(target && target.classList.contains('tabs__item')) {
+            tabs.forEach((item, i) => {
+                if(target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+});
